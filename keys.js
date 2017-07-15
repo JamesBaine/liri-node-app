cab41be7c598ea4fb229926e9b4e0d72b8575131
JamesBaine <jamesmbaine@gmail.com>
2017-07-14 23:48:14 -0700

    initial files created

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '	LLbuTKfiqbZXqBc5RXaSoX8tB',
+  consumer_secret: 'XywvrWKWqUEfByTDtCrioS0NXQjBMZ1OaxSnWsoorNmxiEISQr',
+  access_token_key: '885997569360314368-hjhfWuiKL7k8K0dyXmOvQnPD9zrOIAE',
+  access_token_secret: 'LsoQtfCYTRrjiKoYsJeiHByz7slv9AgpAML0N77FuX2Kc',
+}
